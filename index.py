8ad088f5d150e6e9b9ad9eebc06fad798965731b
Sasin Godar <sago_bca2080@lict.edu.np>
2025-08-19 11:10:11 +0545

    update

---
+++ index.py
@@ -0,0 +1,48 @@
+        # --odd even -------->
+# a = int (input("enter any number :"))
+# if a%2 ==0:
+#       print(f"{a} is even")
+# else:
+#        print(f"{a} is odd")
+
+
+# age = int (input("Enter the age :"))
+
+
+# voting ------------>
+
+# if age<=0:
+#     print("invalid age given")
+# elif age<18:
+    
+#     print("sorry not enough age to vote")
+# else:
+#     print("can go for vote")
+
+
+# greater smaller ----------->
+
+# num1 = int (input("Enter the number 1 :"))
+# num2 = int(input("Enter the number 2 :"))
+
+# if num1 == num2 :
+#     print(f"{num1} and {num2} are equall")
+# elif num1 > num2:
+#     print(f"{num1 } is greater then {num2}")
+# else:
+#     print(f"{num2} is greater then {num1} ")
+
+
+# for loop --------------->
+# for i in range(1,10):
+#     print(i)
+
+for i in range(1,10):
+        print("sasin")
+      
+var = "Lumbini ict campus"
+for i in var:
+        print(i)      
+
+
+
