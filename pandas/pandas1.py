36ae81c364f59b8ea4b7260a088e11914fa99b9a
Sasin Godar <sago_bca2080@lict.edu.np>
2025-11-07 11:48:33 +0545

    update

--- pandas/pandas1.py
+++ pandas/pandas1.py
@@ -44,6 +44,62 @@ df['Salary'] = [75000,45000,23000,32334,20000]
 print("DataFrame with salary column :\n",df)
 
 #filtering data:show the data whose age > 25
-abvage = df["Age"]>25
-print("salary of age above 25 is :\n",pd.where(abvage)[0])
+filter_data = df[df["Age"]>25]
+print("salary of age above 25 is :\n",filter_data)
+
+#using head to view first 5 rows
+print("first 5 rows :\n",df.head())
+
+#using head to view first 3 rows
+print("first 3 rows:\n",df.head(3))
+
+#using tail
+print("last five rows :\n",df.tail())
+
+#for last 3 rows
+print("last 3 rows :\n",df.tail(3))
+print("\n")
+
+#Accessing dataFrame attributes
+
+print("dataframe index :",df.index)
+print("\n")
+print("dataframe column :",df.columns)
+print("\n")
+print("dataframe data types :\n",df.dtypes)
+print("\n")
+print("dataframe shape :",df.shape)
+print("\n")
+print("dataframe size :",df.size)
+print("\n")
+print("dataframe values :",df.values)
+print("\n")
+print("dataframe transpose :\n",df.T)
+print("\n\n")
+print("dataframe info :")
+df.info()
+print("\n")
+
+#For series
+
+ser = pd.Series([1,2,3],index=['a','b','c'], name = "Example Series")
+
+#Accenssing the series 
+
+print("Series index :",ser.index)
+print("\n")
+print("Series data types :\n",ser.dtypes)
+print("\n")
+print("Series shape :",ser.shape)
+print("\n")
+print("Series size :",ser.size)
+print("\n")
+print("Series Name :\n",ser.name)
+print("\n")
+print("Series transpose :\n",ser.is_unique)
+print("\n\n")
+print("Series info :\n",ser.isnull())
+print("\n")
+
+
 
